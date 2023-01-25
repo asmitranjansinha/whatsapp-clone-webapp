a461fb07abf4a7270ccbbdae90e266ca19ef6161
asmy8h <sinhaasmit99@gmail.com>
2023-01-26 03:45:11 +0530

    created sidebar header

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,10 +1,38 @@
+import { ChatRounded, DonutLarge, Filter, Filter1, GroupRounded, MoreVert, Search, StackedBarChart } from '@mui/icons-material'
+import { Avatar, IconButton } from '@mui/material'
+import { grey } from '@mui/material/colors'
 import React from 'react'
 import "./Sidebar.css"
 
 function Sidebar() {
   return (
     <div className='sidebar'>
-      <h1>I am a Sidebar</h1>
+      <div className="sidebar__header">
+
+        <div className="sidebar__headerLeft"><Avatar src="https://avatars.githubusercontent.com/u/101510670?v=4" /></div>
+
+        <div className="sidebar__headerRight">
+          <IconButton><GroupRounded style={{ color: grey[400] }} /></IconButton>
+          <IconButton><DonutLarge style={{ color: grey[400] }} /> </IconButton>
+          <IconButton><ChatRounded style={{ color: grey[400] }} /></IconButton>
+          <IconButton><MoreVert style={{ color: grey[400] }} /></IconButton>
+        </div>
+
+      </div>
+
+      <div className="sidebar__search">
+
+        <div className="sidebar__searchContainer">
+
+          <IconButton><Search style={{ color: grey[400] }} /></IconButton>
+          <input type="text" placeholder='Search or start new chat' />
+
+        </div>
+
+        <IconButton><Filter style={{color: grey[400], fontSize: 18}}/></IconButton>
+
+      </div>
+
     </div>
   )
 }
